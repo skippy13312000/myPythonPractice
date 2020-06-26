a5de0e888fdbbc4a4b9f20d01c16245af737ca46
Brian Skipper <brian.skipper@gmail.com>
2020-06-26 12:41:31 -0400

    https://github.com/skippy13312000/myPythonPractice.git

---
+++ elif.py
@@ -0,0 +1,10 @@
+name = "Alice"
+age = 5
+if name  == "Alice":
+    print("hi alice")
+elif age < 12:
+    print("you're not Alice kiddo")
+elif age > 2000:
+    print("unline you alice is not a vampire")
+elif age > 100:
+    print("Grandma age")

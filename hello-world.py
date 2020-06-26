532bc981dafa7db89d7481318231bcc34e15b3e5
Brian Skipper <brian.skipper@gmail.com>
2020-06-26 09:55:41 -0400

    First commit. Learning how this works.

---
+++ hello-world.py
@@ -0,0 +1,19 @@
+# Simple python hello world program with some input statements and simple arithmatic
+# Author: Brian Skipper
+# Date: 6/26/2020
+
+print("Hello World!")
+
+print("Please input your name: ")
+name = input()
+
+print("Your name is",name)
+
+print("The length of your name is", len(name),"characters!")
+
+print("What is your age?")
+age = input()
+
+print("You're age with be " + str(int(age)+1) + " after you next birthday.")
+
+print("this is test "+str(int(100))+" more stuff")
---
+++ lengthFunctionTest.py
@@ -0,0 +1,8 @@
+# Simple python: messing len()
+# Author: Brian Skipper
+# Date: 6/26/2020
+
+x = "This is a test of len() function."
+
+print(len(x))
+

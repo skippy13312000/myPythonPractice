2418a4821618e9cdfe24cd14bae6c915587d51d2
Brian Skipper <brian.skipper@gmail.com>
2020-06-26 16:47:27 -0400

    https://github.com/skippy13312000/myPythonPractice.git

---
+++ Practice/import.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# importing modules and random number stuff
+# Author: Brian Skipper
+# Date: 6/26/2020
+
+import random
+
+for i in range(10):
+    print(random.randint(1, 10000000))
+    

55e6faf66b5d4dae55e782e03888c41aa74eea75
Brian Skipper <brian.skipper@gmail.com>
2020-06-26 10:57:00 -0400

    https://github.com/skippy13312000/myPythonPractice.git

---
+++ elseIfPractice.py
@@ -0,0 +1,10 @@
+# Simple python: if/else practice
+# Author: Brian Skipper
+# Date: 6/26/2020
+
+name = "Alice"
+
+if name == "Alice":
+    print("Hi Alice")
+else:
+    print("You're name isn't Alice.")

0de1afb25c6166cd66943196546a9c9d99d23818
Brian Skipper <brian.skipper@gmail.com>
2020-06-26 21:09:15 -0400

    https://github.com/skippy13312000/myPythonPractice.git

---
+++ Practice/more-function-stuff.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# More function() stuff
+# Author: Brian Skipper
+# Date: 6/26/2020
+
+def eggs():
+    num_egg = 1000
+    return(num_egg)
+
+print("I have this many eggs: "+str(eggs()))

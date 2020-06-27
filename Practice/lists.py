472632cff592780a63fdd875e9eb439d11b440aa
Brian Skipper <brian.skipper@gmail.com>
2020-06-26 22:10:37 -0400

    https://github.com/skippy13312000/myPythonPractice.git

--- Practice/lists.py
+++ Practice/lists.py
@@ -2,15 +2,25 @@
 # LISTS
 # Author: Brian Skipper
 # Date: 6/26/2020
+import random
 
+#create a list. Lists use [x,x,...,x]
 spam = ["cat", "dog", "fish", "gerbil", "elephant"]
 
+#print out items from the list
 print("First: "+str(spam[2:5])+" and "+spam[1]+"\n1\n2")
 
+#return the length of the list
 print("Length of Spam: "+str(len(spam)))
 
+#print the list out with a 'for loop'
 for x in spam:
     print(x)
 
+#print out the indexes and the corresponding item in the list
 for index, item in enumerate(spam):
     print("Index "+str(index)+" in spam list "+item)
+
+#print out a random item from the list
+print("Random thing from list spam: "+random.choice(spam))
+

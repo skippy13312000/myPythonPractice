ed1b31992b7f5c4530bc9c95d005635e64b97e64
Brian Skipper <brian.skipper@gmail.com>
2020-06-26 20:48:01 -0400

    https://github.com/skippy13312000/myPythonPractice.git

---
+++ Practice/function-math-practice.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Another simple func() practice program
+# Author: Brian Skipper
+# Date: 6/26/2020
+
+def math_stuff(x):
+    number = x * x
+    return number
+    
+
+print("Enter a number: ")
+
+num1 = abs(int(input()))
+
+print("Number returned is "+str(math_stuff(num1)))

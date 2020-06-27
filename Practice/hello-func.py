cb7331dfaaeea02d4f7635c7676a8034969a1ebb
Brian Skipper <brian.skipper@gmail.com>
2020-06-26 20:28:09 -0400

    https://github.com/skippy13312000/myPythonPractice.git

---
+++ Practice/hello-func.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Simple hello()
+# Author: Brian Skipper
+# Date: 6/26/2020
+
+def hello():
+    print("hello!")
+    print("hi there!")
+    print("Hellow function just ran!")
+
+
+hello()

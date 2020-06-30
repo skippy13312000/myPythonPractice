df40b05a9a007cea67add3efdcb10554af05942c
Brian Skipper <brian.skipper@gmail.com>
2020-06-30 18:22:40 -0400

    master

--- Practice/copy_func.py
+++ Practice/copy_func.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-# LISTS
+# this is function practice to show the id() output and how it does and doesn't change
 # Author: Brian Skipper
 # Date: 6/26/2020
 
 import copy
 spam = ['a','b','c','d']
 
-print(id(spam))
-
-cheese=copy.copy(spam)
-print(id(cheese))
-
-cheese[1] = 42
-
+print("ID:       "+str(id(spam)))
 print(spam)
 
+cheese=copy.copy(spam)
+print("Cheese:   "+str(id(cheese)))
+cheese[1] = 42
 print(cheese)
+
+

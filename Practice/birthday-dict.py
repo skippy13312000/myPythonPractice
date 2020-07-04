6f75a4e184e37072333b67da26b362a908e92c6a
Brian Skipper <brian.skipper@gmail.com>
2020-07-04 14:15:03 -0400

    https://github.com/skippy13312000/myPythonPractice.git

---
+++ Practice/birthday-dict.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# dict and list practice
+# Author: Brian Skipper
+# Date: 6/26/2020
+
+birthdays = {'Brian':'Nov 26','Holly':'Apr 27','Dina':'Mar 6'}
+
+while True:
+    print("Enter a name: (blank to quit)")
+    name=input()
+    if name == "":
+        break
+    if name in birthdays:
+        print(birthdays[name]+" is the birthday of "+ name)
+    else:
+        print("i do not have a birthday for "+name)
+        print("What is the bithday?") 
+        bday=input()
+        birthdays[name] = bday
+        print("Birthdays updated")
+       

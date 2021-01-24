1a662e5ea8ef15e4254b3d45d462b3390814bb13
Brian Skipper <brian.skipper@gmail.com>
2021-01-24 09:19:53 -0500

    https://github.com/skippy13312000/myPythonPractice.git

--- Practice/lengthFunctionTest.py
+++ Practice/lengthFunctionTest.py
@@ -4,7 +4,7 @@
 # Date: 6/26/2020
 
 x = "This is a test of len() function."
-
-print(len(x))
+print(x)
+print(len(x), "Is the length of the string held in the X variable.")
 
 #let's take this out so we have a change to commit through GIT
